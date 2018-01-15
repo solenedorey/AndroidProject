fe6d81ecb84dc1dca1a0b9653ccea007161ce2cf
solenedorey <solene.dorey@outlook.com>
2018-01-15 16:24:32 +0100

    Home page design in progress

--- app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
@@ -1,7 +1,9 @@
 package dnr2i.master.unicaen.fr.androidproject;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        actionBar.hide();
     }
 }
