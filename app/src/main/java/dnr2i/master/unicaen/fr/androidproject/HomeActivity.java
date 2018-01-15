7fd78088d230ecc4c5f0035b634ba1e5c90686d7
solenedorey <solene.dorey@outlook.com>
2018-01-15 15:25:10 +0100

    Home page in progress

--- app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
@@ -3,11 +3,11 @@ package dnr2i.master.unicaen.fr.androidproject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
