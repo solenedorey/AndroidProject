42409cb4e6d7c6b610b281d03ddb5dc01bc77488
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-15 16:25:22 +0100

    Add intent + some form fields.

---
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/AddActivity.java
@@ -0,0 +1,13 @@
+package dnr2i.master.unicaen.fr.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/HomeActivity.java
@@ -1,7 +1,9 @@
 package dnr2i.master.unicaen.fr.androidproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void add(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
     }
 }
