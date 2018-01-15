37ff62f739036bf05b20a3c64f24f5200f8255c2
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-15 17:53:26 +0100

    Finalize add form's structure + display all data after submit.

--- app/src/main/java/dnr2i/master/unicaen/fr/androidproject/AddActivity.java
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/AddActivity.java
@@ -2,6 +2,8 @@ package dnr2i.master.unicaen.fr.androidproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddActivity extends Activity {
 
@@ -9,5 +11,38 @@ public class AddActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+
+    public void submit(View view) {
+        String title = ((EditText) findViewById(R.id.addTtitleField)).getText().toString();
+
+        double price;
+        try {
+            price = Double.parseDouble(((EditText) findViewById(R.id.addPriceField)).getText().toString());
+        } catch (NumberFormatException e) {
+            price = -1;
+        }
+
+        String description = ((EditText) findViewById(R.id.addDescriptionField)).getText().toString();
+        String email = ((EditText) findViewById(R.id.addEmailField)).getText().toString();
+        String pseudo = ((EditText) findViewById(R.id.addPseudoField)).getText().toString();
+        String phone = ((EditText) findViewById(R.id.addPhoneField)).getText().toString();
+        String city = ((EditText) findViewById(R.id.addCityField)).getText().toString();
+
+        int postcode;
+        try {
+            postcode = Integer.parseInt(((EditText) findViewById(R.id.addPostcodeField)).getText().toString());
+        } catch (NumberFormatException e) {
+            postcode = -1;
+        }
+
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(description);
+        System.out.println(email);
+        System.out.println(pseudo);
+        System.out.println(phone);
+        System.out.println(city);
+        System.out.println(postcode);
     }
 }
