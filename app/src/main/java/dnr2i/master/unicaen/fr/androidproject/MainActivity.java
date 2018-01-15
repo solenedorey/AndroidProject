21ffcf6dc54a59b92c0d891d1e70fbec1ef6c2b8
solenedorey <solene.dorey@outlook.com>
2018-01-15 14:39:54 +0100

    First Commit

---
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/MainActivity.java
@@ -0,0 +1,13 @@
+package dnr2i.master.unicaen.fr.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
