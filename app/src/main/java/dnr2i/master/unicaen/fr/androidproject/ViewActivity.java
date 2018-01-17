2cd17ad818e700c88dd4e6310d2e0b89c49e79ee
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-17 11:23:19 +0100

    Display ad data in ViewActivity.

--- app/src/main/java/dnr2i/master/unicaen/fr/androidproject/ViewActivity.java
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/ViewActivity.java
@@ -2,6 +2,7 @@ package dnr2i.master.unicaen.fr.androidproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -9,6 +10,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ViewActivity extends Activity {
 
@@ -18,13 +26,47 @@ public class ViewActivity extends Activity {
         setContentView(R.layout.activity_view);
 
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext()); // ou this
-        String url = "https://ensweb.users.info.unicaen.fr/android-api/?method=randomAd";
+        String url = "https://ensweb.users.info.unicaen.fr/android-api/?apikey=dnr2&method=randomAd";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener() {
                     @Override
                     public void onResponse(Object response) {
                         System.out.println(response.toString());
+
+                        JSONObject json = null;
+                        try {
+                            json = new JSONObject(response.toString());
+                            JSONObject adData = (JSONObject) json.get("response");
+                            Ad ad = new Ad(
+                                    adData.getString("id"),
+                                    adData.getString("titre"),
+                                    adData.getString("description"),
+                                    adData.getDouble("prix"),
+                                    adData.getString("pseudo"),
+                                    adData.getString("emailContact"),
+                                    adData.getString("telContact"),
+                                    adData.getString("ville"),
+                                    adData.getString("cp"),
+                                    adData.getInt("date")
+                            );
+                            ((TextView) findViewById(R.id.viewTitle)).setText(ad.getTitle());
+                            ((TextView) findViewById(R.id.viewDescription)).setText(ad.getDescription());
+                            ((TextView) findViewById(R.id.viewPrice)).setText(String.valueOf(ad.getPrice()));
+                            ((TextView) findViewById(R.id.viewPseudo)).setText(ad.getPseudo());
+                            ((TextView) findViewById(R.id.viewEmail)).setText(ad.getEmail());
+                            ((TextView) findViewById(R.id.viewPhone)).setText(ad.getPhone());
+                            ((TextView) findViewById(R.id.viewCity)).setText(ad.getCity());
+                            ((TextView) findViewById(R.id.viewPostcode)).setText(ad.getPostcode());
+
+                            Date date = new Date(ad.getDate() * 1000L);
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                            ((TextView) findViewById(R.id.viewDate)).setText(dateFormat.format(date));
+                            System.out.println(ad);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
