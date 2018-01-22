c2d1c015e5ed462297ddffe7a1735205d3cb4dc4
solenedorey <solene.dorey@outlook.com>
2018-01-22 08:32:20 +0100

    List ads fonctionnality started commit 2

---
+++ app/src/main/java/dnr2i/master/unicaen/fr/androidproject/ListActivity.java
@@ -0,0 +1,22 @@
+package dnr2i.master.unicaen.fr.androidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
